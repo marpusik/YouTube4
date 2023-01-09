6e723498b6c105ea59d3e87477e333372112a821
marpusik <marpusik1987@gmail.com>
2023-01-09 09:22:48 +0300

    7

--- src/js/main.js
+++ src/js/main.js
@@ -31,9 +31,12 @@ burgerMenu()
 function submenu() {
   const submenu = document.querySelector('.submenu')
   const trigger = document.querySelector('.with__submenu')
+  const triggerLink = document.querySelector('.trigger__link')
   const arrow = document.querySelector('.submenu__arrow')
   trigger.addEventListener('click', () => {
     submenu.classList.toggle('active')
+    triggerLink.classList.toggle('active')
+    arrow.classList.toggle('active')
   })
 }
 submenu()
